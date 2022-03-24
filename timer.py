36458ce190cb6e0db57af5f7ba9b8e1216014abe
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-03-24 19:37:51 +0000

    Basic project layout

---
+++ timer.py
@@ -0,0 +1,26 @@
+"""Simple project timer"""
+
+import datetime
+
+
+class Timer:
+    def __init__(self, activity=None):
+        self.activity = activity
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+    def record(self, event):
+        pass
+
+    def new_activity(self):
+        pass
+
+    def read_record(self, event):
+        pass
+
+    def cmdline(self):
+        pass
